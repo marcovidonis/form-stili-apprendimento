715645002eda2ae39a8ccc7774d2ac986f20cae7
Marco Vidonis <marco.vidonis@gmail.com>
2022-09-04 15:29:27 +0100

    fix form reset

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
   const [results, setResults] = useState(initialResults);
 
   const restartForm = () => {
-    setResults(initialResults);
+    setResults({ ...initialResults });
     setShowResults(false);
   };
 
